bf91ca2ef558523ce495485afea43a1682115b02
dorothyrm <36931572+dorothyrm@users.noreply.github.com>
2018-03-28 17:13:01 -0400

    Add files via upload

---
+++ DataAnalysis.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class DataAnalysis {
+	public static void main(String[] args) {
+		
+		DataAnalysis myreader =  new DataAnalysis();
+	 myreader.readRedditFileForPositive("redditPosts.txt");
+	 myreader.readRedditFileForNegative("redditPosts.txt");
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ RedditPosts.java
@@ -0,0 +1,11 @@
+public class RedditPosts {
+	
+	String title;
+
+	
+	RedditPosts(String postname)
+	{
+		title=postname;
+	}
+
+}
